ca1d8bb72ee90d5cf70aa9d046f6f82ba1c79c52
Leo Villanueva <leo@Leos-MBP-2.lan>
2026-02-12 23:06:46 -0800

    comments for node.py

--- node.py
+++ node.py
@@ -1,12 +1,12 @@
-class Node: 
-    def __init__(self, state, g, h, parent):
+class Node:                                         
+    def __init__(self, state, g, h, parent):        #initialize node                      
         self.state = state 
         self.g = g
         self.h = h; 
         self.parent = parent
-        self.f = g + h
+        self.f = g + h                              #A* est cost
 
-    def __lt__(self, other):
+    def __lt__(self, other):                        #priority queue comparison 
         return self.f < other.f
 
 
